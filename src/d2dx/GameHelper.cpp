aa5d689ac8b51ead15b13552cafc775c13cde2a2
bolrog <81111887+bolrog@users.noreply.github.com>
2021-06-03 08:14:00 +0200

    Fix in-game detection for 1.10f.

--- src/d2dx/GameHelper.cpp
+++ src/d2dx/GameHelper.cpp
@@ -1199,6 +1199,8 @@ bool GameHelper::IsInGame() const
 	{
 	case GameVersion::Lod109d:
 		return *((uint32_t*)((uint32_t)_hD2ClientDll + 0x1109FC)) != 0 && playerUnit != 0 && playerUnit->u.v109.path != 0;
+	case GameVersion::Lod110:
+		return *((uint32_t*)((uint32_t)_hD2ClientDll + 0x1077C4)) != 0 && playerUnit != 0 && playerUnit->u.v109.path != 0;
 	case GameVersion::Lod112:
 		return *((uint32_t*)((uint32_t)_hD2ClientDll + 0x11BCC4)) != 0 && playerUnit != 0 && playerUnit->u.v112.path != 0;
 	case GameVersion::Lod113c:
