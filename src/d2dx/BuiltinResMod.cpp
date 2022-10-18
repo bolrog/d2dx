2163af237debe33ea8c10cee2779d9a34899a292
Jason Newcomb <jsnewcomb@pm.me>
2022-10-18 15:49:03 -0400

    Load resolution mod on 1.10f and 1.12

--- src/d2dx/BuiltinResMod.cpp
+++ src/d2dx/BuiltinResMod.cpp
@@ -78,6 +78,8 @@ bool BuiltinResMod::IsCompatible(
     auto gameVersion = gameHelper->GetVersion();
 
     if (gameVersion != d2dx::GameVersion::Lod109d &&
+        gameVersion != d2dx::GameVersion::Lod110f &&
+        gameVersion != d2dx::GameVersion::Lod112 &&
         gameVersion != d2dx::GameVersion::Lod113c &&
         gameVersion != d2dx::GameVersion::Lod113d &&
         gameVersion != d2dx::GameVersion::Lod114d)
