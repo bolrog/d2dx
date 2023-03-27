a95db00606dd07f954dfe54d175e183e21bb00ed
Jason Newcomb <jsnewcomb@pm.me>
2023-03-27 11:34:21 -0400

    Limit cursor clipping to the render area

--- src/d2dx/RenderContext.cpp
+++ src/d2dx/RenderContext.cpp
@@ -400,7 +400,7 @@ void RenderContext::Present()
 	UpdateViewport(_renderRect);
 
 	RenderContextPixelShader pixelShader;
-	
+
 	switch (_d2dxContext->GetOptions().GetFiltering())
 	{
 	default:
@@ -1026,11 +1026,15 @@ void RenderContext::ClipCursor()
 		return;
 	}
 
-	RECT clipRect;
-	::GetClientRect(_hWnd, &clipRect);
+	RECT clipRect = {
+		_renderRect.offset.x,
+		_renderRect.offset.y,
+		_renderRect.offset.x + _renderRect.size.width,
+		_renderRect.offset.y + _renderRect.size.height
+	};
 	::ClientToScreen(_hWnd, (LPPOINT)&clipRect.left);
-	::ClientToScreen(_hWnd, (LPPOINT)&clipRect.right);
-	::ClipCursor(&clipRect);
+    ::ClientToScreen(_hWnd, (LPPOINT)&clipRect.right);
+    ::ClipCursor(&clipRect);
 }
 
 void RenderContext::UnclipCursor()
